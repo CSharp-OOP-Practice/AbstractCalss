f778dfea9fb1c21313c888c7f3fc41ad2f6a3ea9
RayW <ruizhouwu@gmail.com>
2018-03-11 22:09:48 +0800

    init

---
+++ AbstractCalss/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbstractCalss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>();
+
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+
+            foreach (var shape in shapes)
+                Console.WriteLine(shape.GetShape());
+        }
+    }
+}
---
+++ AbstractCalss/Shape.cs
@@ -0,0 +1,19 @@
+﻿namespace AbstractCalss
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public abstract void Draw();
+
+        protected int GetCount()
+        {
+            return 1;
+        }
+
+        public string GetShape()
+        {
+            return "This is shape.";
+        }
+    }
+}
